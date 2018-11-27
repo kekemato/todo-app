4e26d027cdb5667139d4f23ea76ed272c18ae009
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-27 14:54:10 +0100

    Updates

---
+++ precache-manifest.7038953dca045cc6dd6dae195b56285c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "1346f7d448ac6381d736",
+    "url": "/todo-app/static/js/main.1346f7d4.chunk.js"
+  },
+  {
+    "revision": "bfa9ee2f999378492a05",
+    "url": "/todo-app/static/js/1.bfa9ee2f.chunk.js"
+  },
+  {
+    "revision": "1346f7d448ac6381d736",
+    "url": "/todo-app/static/css/main.fcb7d797.chunk.css"
+  },
+  {
+    "revision": "4a20a3e8720a92821e9338304abf9978",
+    "url": "/todo-app/index.html"
+  }
+];
